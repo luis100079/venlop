d1224303e78c63dfd680ea544ee136de2a3dbf56
Luis Rene <luislopez100079@gmail.com>
2020-05-28 19:45:38 -0600

    before adding vuex

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app id='app'>
+
+
+
+  </v-app>
+</template>
+
+<script>
+
+export default {
+
+    data: () => ({
+        justify: ['start', 'end', 'center', 'space-between', 'space-round']
+    })
+
+}
+
+</script>
